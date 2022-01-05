20492f7169a7e419fff4c2b9b24c5503aeb82efd
Victor Cordos <cordos.victor@gmail.com>
2022-01-05 20:54:41 +0200

    feat: dao for our database client

---
+++ src/services/c1Schemas.ts
@@ -0,0 +1,8 @@
+export interface ClassQuerySchema {
+    ClassUUID: string
+    ClassName: string
+    ClassShortCode: string
+    OrganizationName: string
+    SchoolName: string
+    ProgramName: string[]
+}
