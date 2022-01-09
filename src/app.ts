3ef9b31eefb7589490dce7f5f9ce6c2b44164600
Naaman Curtis <ncurtis@calmid.com>
2022-01-09 07:07:33 -0700

    feat(api): add docs and swagger hosting

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,14 @@ import { v1Router } from './routes/v1';
 import { v4 as uuidv4 } from 'uuid';
 import { Redis } from './utils/redis';
 import { config } from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 config();
 
-const API_KEY = process.env.API_KEY;
+const swaggerDocs = YAML.load('./api-docs.yaml');
+
+export const API_KEY = process.env.API_KEY;
 if (!API_KEY || API_KEY.length < 3)
   throw new Error(`Must provide an API key to the application`);
 
@@ -29,12 +33,11 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     path: req.path,
     method: req.method,
   });
-  if (req.header('X-Api-Key') !== API_KEY)
-    throw createError(401, 'Unauthorized Request');
   next();
 });
 
 app.use('/v1', v1Router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -47,7 +50,7 @@ app.use((err: HttpError, req: Request, res: Response, nex: NextFunction) => {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   res.status(err.status || 500);
-  res.send(`error ${err.status}: ${err.message}`);
+  res.json({ code: err.status, message: err.message });
 });
 
 async function main() {
--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,26 +1,39 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { v4 as uuidv4 } from 'uuid';
-import { body, Meta } from 'express-validator';
+import { body, Meta, validationResult } from 'express-validator';
+import createError from 'http-errors';
 import { Redis } from '../utils/redis';
 import { Message } from '../utils/message';
 import { Entity } from '../entities';
+import { API_KEY } from '../app';
+import { validate as validateUuid } from 'uuid';
 
 const router = Router();
 
-const arrayValidator =
-  (minLengthForEachElement = 3) =>
-  (input: unknown, _: Meta) => {
-    if (!Array.isArray(input)) return false;
-    for (const e of input) {
-      if (typeof e !== 'string') return false;
-      if (e.length < minLengthForEachElement) return false;
-    }
-    return true;
-  };
+const arrayValidator = (input: unknown, _: Meta) => {
+  if (!Array.isArray(input))
+    return Promise.reject('Expected an array of UUIDs');
+  for (let i = 0; i < input.length; i++) {
+    const id = input[i];
+    if (typeof id !== 'string')
+      return Promise.reject(
+        `Expected each UUID to be in string format.\nElement at index ${i} was of type: ${typeof id}`
+      );
+    if (!validateUuid(id))
+      return Promise.reject(`Element ${id} at index ${i} is not a valid UUID`);
+  }
+  return true;
+};
 
 const defaultBodyValidator = body('ids').custom(arrayValidator);
 
-router.post('/organization/all', async (req: Request, res: Response) => {
+router.use((req: Request, _: Response, next: NextFunction) => {
+  if (req.header('X-Api-Key') !== API_KEY)
+    throw createError(401, 'Unauthorized Request');
+  next();
+});
+
+router.post('/process-all', async (req: Request, res: Response) => {
   const trace = (req.headers.trace as string) || uuidv4();
   const redis = await Redis.initialize();
   const msg = new Message(Entity.ORGANIZATION, 'ALL', trace, 0, true, true);
@@ -32,8 +45,7 @@ router.post(
   '/organization',
   defaultBodyValidator,
   async (req: Request, res: Response) => {
-    const response = await processRequest(Entity.ORGANIZATION, req, res);
-    response.send();
+    await processRequest(Entity.ORGANIZATION, req, res);
   }
 );
 
@@ -41,8 +53,7 @@ router.post(
   '/school',
   defaultBodyValidator,
   async (req: Request, res: Response) => {
-    const response = await processRequest(Entity.SCHOOL, req, res);
-    response.send();
+    await processRequest(Entity.SCHOOL, req, res);
   }
 );
 
@@ -50,8 +61,7 @@ router.post(
   '/class',
   defaultBodyValidator,
   async (req: Request, res: Response) => {
-    const response = await processRequest(Entity.CLASS, req, res);
-    response.send();
+    await processRequest(Entity.CLASS, req, res);
   }
 );
 
@@ -59,12 +69,14 @@ router.post(
   '/user',
   defaultBodyValidator,
   async (req: Request, res: Response) => {
-    const response = await processRequest(Entity.USER, req, res);
-    response.send();
+    await processRequest(Entity.USER, req, res);
   }
 );
 
 async function processRequest(entity: Entity, req: Request, res: Response) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
   const trace = (req.headers.trace as string) || uuidv4();
   const cascade = shouldCascade(req);
 
@@ -81,7 +93,10 @@ async function processRequest(entity: Entity, req: Request, res: Response) {
 }
 
 function shouldCascade(req: Request): boolean {
-  if (req.params.cascade == 'true') return true;
+  const cascade = req.query.cascade;
+  if (cascade === undefined) return false;
+  if (typeof cascade === 'string' && cascade === 'true') return true;
+  if (typeof cascade === 'boolean' && cascade) return true;
   return false;
 }
 
