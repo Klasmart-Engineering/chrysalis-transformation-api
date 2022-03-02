7afe1648f1ba13f56ee74100bedc1b61c1c57cce
ionutCristian805 <ionut.rusu@endava.com>
2022-03-02 14:23:19 +0200

    fix: remove useless loweCase

--- src/services/backendService.ts
+++ src/services/backendService.ts
@@ -147,7 +147,7 @@ export class BackendService {
       requestMeta.setId(requestIds.CREATE_CLASS).setN('1');
 
       classProto
-        .setExternalUuid(clazz.ClassUUID.toLowerCase())
+        .setExternalUuid(clazz.ClassUUID)
         .setName(clazz.ClassName)
         .setExternalOrganizationUuid(clazz.OrganizationUUID)
         .setExternalSchoolUuid(clazz.SchoolUUID);
@@ -242,7 +242,7 @@ export class BackendService {
 
     requestMeta.setId(requestIds.ADD_PROGRAMS_TO_CLASS).setN(n);
     addProgramsToClass
-      .setExternalClassUuid(clazz.ClassUUID.toLowerCase())
+      .setExternalClassUuid(clazz.ClassUUID)
       .setProgramNamesList(clazz.ProgramName);
 
     linkPrograms.setAddProgramsToClass(addProgramsToClass);
